20da351b06869b873b6df5377362bcc4b4a6cd87
Purwa Darozatun <purwa@javan.co.id>
2016-07-24 18:02:44 +0700

    Add Migration Controller

---
+++ application/migrations/20160724105628_coba.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Coba extends CI_Migration {
+
+        public function up()
+        {
+            //Up Migration Goes Here
+        }
+
+        public function down()
+        {
+            //Down Migration Goes Here
+        }
+}
